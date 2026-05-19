75bf7c9ee571a0952d2db4535033b1a65a95e333
Ben-Butcher <benhtembe@gmail.com>
2026-05-19 18:30:47 +0200

    Add some more contect

--- script/app.js
+++ script/app.js
@@ -1,1 +1,18 @@
-console.log("We are live!");
+const title = ["My name is Ben", "I am a full stack developer"];
+const body = document.querySelector("body");
+
+window.addEventListener("load", () => {
+  const toggleContainer = document.querySelector(".toggle");
+
+  toggleContainer.addEventListener("click", () => {
+    if (!toggleContainer.children[0].classList.contains("hide")) {
+      toggleContainer.children[0].classList.add("hide");
+      toggleContainer.children[1].classList.remove("hide");
+      body.classList.add("dark-mode");
+    } else {
+      toggleContainer.children[0].classList.remove("hide");
+      toggleContainer.children[1].classList.add("hide");
+      body.classList.remove("dark-mode");
+    }
+  });
+});
