08bd44411515f2b7ea21dc03934d6ee5e78b8ff2
Ben-Butcher <benhtembe@gmail.com>
2026-05-30 22:04:15 +0200

    I have made some changes

--- script/app.js
+++ script/app.js
@@ -1,18 +1,3 @@
-const title = ["My name is Ben", "I am a full stack developer"];
-const body = document.querySelector("body");
+const modeToggleContainer = document.querySelector(".mode_toggle_container");
 
-window.addEventListener("load", () => {
-  const toggleContainer = document.querySelector(".toggle");
-
-  toggleContainer.addEventListener("click", () => {
-    if (!toggleContainer.children[0].classList.contains("hide")) {
-      toggleContainer.children[0].classList.add("hide");
-      toggleContainer.children[1].classList.remove("hide");
-      body.classList.add("dark-mode");
-    } else {
-      toggleContainer.children[0].classList.remove("hide");
-      toggleContainer.children[1].classList.add("hide");
-      body.classList.remove("dark-mode");
-    }
-  });
-});
+modeToggleContainer.children.item;
