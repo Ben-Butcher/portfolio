6a7889557a6b3307cf8e868dcfc8abd629b6b65e
Ben-Butcher <benhtembe@gmail.com>
2026-05-31 01:56:21 +0200

    Made more changes

--- script/app.js
+++ script/app.js
@@ -3,15 +3,18 @@ const darkMode = document.getElementById("dark-mode");
 const heroH1 = document.getElementById("hero");
 const pageNavigation = document.querySelector(".page-navigation");
 const modeToggleContainer = document.querySelector(".mode-toggle-container");
-
+const body = document.getElementById("body");
+const mainContainer = document.getElementById("main");
 // pages objects
 const heroPageObj = {
   title: "Ben Tembe",
+  description:
+    "I write code to make the lives of people who can't write code easier",
 };
 window.addEventListener("load", () => {
   const m = localStorage.getItem("darkMode");
   if (m !== null) {
-    document.getElementById("body").classList.add(m);
+    body.classList.add(m);
     lightMode.classList.add("hide");
     darkMode.classList.remove("hide");
   }
@@ -22,7 +25,7 @@ pageNavigation.childNodes.forEach((element) => {
   element.addEventListener("click", (x) => {
     switch (x.target.attributes[1].nodeValue) {
       case "home":
-        heroH1.innerText = pages[0].title;
+        setHeroPage(heroPageObj);
         break;
       case "projects":
         heroH1.innerText = pages[1].title;
@@ -43,9 +46,24 @@ function changeMode() {
   if (lightMode.classList.contains("hide")) {
     localStorage.setItem("darkMode", "dark-mode");
     const m = localStorage.getItem("darkMode");
-    document.getElementById("body").classList.add(m);
+    body.classList.add(m);
   } else {
-    document.getElementById("body").classList.remove("dark-mode");
+    body.classList.remove("dark-mode");
     window.localStorage.removeItem("darkMode");
   }
+}
+
+function setHeroPage(obj) {
+  const heroContainer = document.createElement("div");
+  const h1El = document.createElement("h1");
+  const pEl = document.createElement("p");
+
+  h1El.innerText = obj.title;
+  pEl.innerText = obj.description;
+
+  heroContainer.classList.add("hero-container");
+  heroContainer.appendChild(h1El);
+  heroContainer.appendChild(pEl);
+
+  mainContainer.append(heroContainer);
 }
